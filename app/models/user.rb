a65b1d5e9831946e0e9cc2eff7746868e34f564d
julie.ps.hsu <julie.ps.hsu@gmail.com>
2023-06-03 13:44:37 +0800

    add dependent:destroy in user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   validates :name, presence: true, uniqueness: {case_sensitive: false}
 
-  has_many :sleep_records
-  has_many :followings, class_name: "FollowingRecord", foreign_key: 'user_id'
+  has_many :sleep_records, dependent: :destroy
+  has_many :followings, class_name: "FollowingRecord", foreign_key: 'user_id', dependent: :destroy
 
   def last_active_sleep_record
   	sleep_records.where("is_finished = false").first
