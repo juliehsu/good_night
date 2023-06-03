16272b634d78072c68770256e91699574c0a4da5
julie.hsu <julie.hsu@gogox.com>
2023-06-03 14:13:15 +0800

    in case, add one more default value

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -2,7 +2,10 @@ class Api::ApplicationController < ::ApplicationController
 	skip_before_action :verify_authenticity_token
 
   def current_user
-    return nil unless session[:user_id]
-    User.find(session[:user_id])
+    #return nil unless session[:user_id]
+    #User.find(session[:user_id])
+    #not implement auth, use config for demo switch current_user
+
+    User.find(::Configuration.find_by_key('session_user_id')&.value) || User.first
   end
 end
